54ce5e25b94f0730d5282abc9eb4b976f6e4e019
markrudenkov <mark.rudenkov@gmail.com>
2016-08-04 18:33:28 +0300

    User controller

--- src/main/java/lt/tieto/msi2016/user/controller/UserController.java
+++ src/main/java/lt/tieto/msi2016/user/controller/UserController.java
@@ -1,12 +1,10 @@
 package lt.tieto.msi2016.user.controller;
 
-import lt.tieto.msi2016.item.model.Item;
+import lt.tieto.msi2016.roles.Roles;
 import lt.tieto.msi2016.user.model.User;
 import lt.tieto.msi2016.utils.controller.BaseController;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by it11 on 16.8.3.
@@ -16,10 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController  extends BaseController {
 
+
+
     @RequestMapping(method = RequestMethod.POST, path = "/api/user")
-    public User createItem(@RequestBody User user) {
+    public User registration(@RequestBody User user) {
         System.out.println(user);
         return user;
     }
+
+
+
+
+    @Secured(Roles.USER)
+    @RequestMapping(method = RequestMethod.GET, path = "/api/user/{id}")
+    public User getUserData(@PathVariable Long id) {
+        return id;
+    }
+
+
+
+
+
 
 }
