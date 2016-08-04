8a9f280720b662ad1607ed21da487a0563d3856e
Darius Turcinskas <turcinskas.d@gmail.com>
2016-08-04 18:53:48 +0300

    Merge branch 'master' of github.com:tieto-lt/javainiai
    # Conflicts:
    #	src/main/java/lt/tieto/msi2016/user/controller/UserController.java

--- src/main/java/lt/tieto/msi2016/user/controller/UserController.java
+++ src/main/java/lt/tieto/msi2016/user/controller/UserController.java
@@ -1,10 +1,15 @@
 package lt.tieto.msi2016.user.controller;
 
-import lt.tieto.msi2016.roles.Roles;
+import lt.tieto.msi2016.item.model.Item;
+import lt.tieto.msi2016.item.service.ItemService;
 import lt.tieto.msi2016.user.model.User;
+import lt.tieto.msi2016.user.service.UserService;
 import lt.tieto.msi2016.utils.controller.BaseController;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by it11 on 16.8.3.
@@ -14,26 +19,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController  extends BaseController {
 
-
+    @Autowired
+    private UserService service;
 
     @RequestMapping(method = RequestMethod.POST, path = "/api/user")
-    public User createItem(@RequestBody User user) {
-        System.out.println(user);
-
+    public User createUser(@RequestBody User user) {
+        service.createUser(user);
         return user;
     }
-
-
-
-
-    @Secured(Roles.CUSTOMER)
-    @RequestMapping(method = RequestMethod.GET, path = "/api/user/{id}")
-    public User getUserData(@PathVariable Long id) {
-        return id;
-    }
-
-
-
 
 
 
