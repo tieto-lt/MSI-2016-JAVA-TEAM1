a785c60b3a137199dbc0a5ff1bc0545e54b55803
Darius Turcinskas <turcinskas.d@gmail.com>
2016-08-08 17:38:53 +0300

    [13][US-5] Create roles management backend

--- src/main/app/admin/services/role_service.js
+++ src/main/app/admin/services/role_service.js
@@ -5,6 +5,10 @@ function Service ($http) {
     this.all = function() {
         return $http.get('/api/roles');
     };
+
+    this.update = function(role) {
+        return $http.put('/api/roles/', role);
+    };
 }
 
 Service.$inject = ['$http'];
