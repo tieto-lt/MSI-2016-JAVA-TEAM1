d28c579b0045f56cec8727c66cfb55b7421a1636
Darius Turcinskas <turcinskas.d@gmail.com>
2016-08-10 08:29:05 +0300

    [20][US-7] Create endpoint which returns mission results

--- src/main/java/lt/tieto/msi2016/mission/controller/MissionResultsController.java
+++ src/main/java/lt/tieto/msi2016/mission/controller/MissionResultsController.java
@@ -1,5 +1,31 @@
 package lt.tieto.msi2016.mission.controller;
 
 
-public class MissionResultsController {
+import lt.tieto.msi2016.item.model.Item;
+import lt.tieto.msi2016.item.service.ItemService;
+import lt.tieto.msi2016.mission.model.MissionResultInternal;
+import lt.tieto.msi2016.mission.service.MissionService;
+import lt.tieto.msi2016.utils.controller.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class MissionResultsController extends BaseController {
+
+    @Autowired
+    private MissionService service;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/api/internalMissions/{id}")
+    public MissionResultInternal get(@PathVariable Long id) {
+        return service.get(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/api/internalMissions")
+    public List<MissionResultInternal> all() {
+        return service.all();
+    }
+
 }
