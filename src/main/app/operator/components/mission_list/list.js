249a4f6094ed96e3a1c0cfb9e37b7bacd4918256
Monika Gasparaite <monika9623@gmail.com>
2016-08-11 19:20:18 +0300

    Photos

--- src/main/app/operator/components/mission_list/list.js
+++ src/main/app/operator/components/mission_list/list.js
@@ -1,12 +1,17 @@
 var module = require('main_module');
 
-function Controller( $scope, MissionService) {
+function Controller($scope, MissionService) {
 
     var vm = this;
     vm.missions = {};
     vm.information = {};
     vm.results = {};
     $scope.oneAtATime = true;
+
+
+      vm.myInterval = 3000;
+      vm.noWrapSlides = false;
+      vm.activeSlide = 0;
 
     vm.$onInit = function() {
             _loadList();
@@ -19,6 +24,7 @@ function Controller( $scope, MissionService) {
                 vm.missions = response.data;
                 console.log(vm.missions);
                 vm.missions.forEach(function (mission) {
+
                     mission.startNavigationData.x = mission.startNavigationData.x || 0;
                     mission.startNavigationData.y = mission.startNavigationData.y || 0;
                     mission.startNavigationData.z = mission.startNavigationData.z || 0;
@@ -46,3 +52,4 @@ module.component('missionList', {
     controller: Controller,
     templateUrl: require('./list.html')
 });
+
