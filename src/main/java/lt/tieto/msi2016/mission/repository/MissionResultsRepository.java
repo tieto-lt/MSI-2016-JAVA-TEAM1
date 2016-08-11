0d4f56697f52224ff254f8cf5fc234b084ad64a9
Darius Turcinskas <turcinskas.d@gmail.com>
2016-08-11 09:17:42 +0300

    [BUG] connect [US-7] DB and backend layers

--- src/main/java/lt/tieto/msi2016/mission/repository/MissionResultsRepository.java
+++ src/main/java/lt/tieto/msi2016/mission/repository/MissionResultsRepository.java
@@ -17,7 +17,7 @@ public class MissionResults extends BaseRepository<MissionResultsDb> {
         item.setExecutedBy(rs.getLong("executed_by"));
         item.setExecutionDate(rs.getString("execution_date"));
         item.setBatteryStatus(rs.getBigDecimal("battery_status"));
-        item.setVideoBase64(rs.getString("video_base_64"));
+        item.setVideoBase64(rs.getString("video_base64"));
         item.setImages(rs.getString("images"));
         item.setNavigationData(rs.getString("navigation_data"));
         item.setMissionState(MissionResultsDb.MissionState.valueOf(rs.getString("mission_state")));
@@ -31,7 +31,7 @@ public class MissionResults extends BaseRepository<MissionResultsDb> {
             "executed_by", MissionResultDb.getExecutedBy(),
             "execution_date", MissionResultDb.getExecutionDate(),
             "battery_status", MissionResultDb.getBatteryStatus(),
-            "video_base_64", MissionResultDb.getVideoBase64(),
+            "video_base64", MissionResultDb.getVideoBase64(),
             "images", MissionResultDb.getImages(),
             "navigation_data", MissionResultDb.getNavigationData(),
             "mission_state", MissionResultDb.getMissionState()
