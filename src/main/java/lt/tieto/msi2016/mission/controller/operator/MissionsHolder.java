d5b682b0bf748852d7be241bdd37351fe8552d88
Darius Turcinskas <turcinskas.d@gmail.com>
2016-08-12 11:04:00 +0300

    add pictures mission for operator

--- src/main/java/lt/tieto/msi2016/mission/controller/operator/MissionsHolder.java
+++ src/main/java/lt/tieto/msi2016/mission/controller/operator/MissionsHolder.java
@@ -14,8 +14,9 @@ public class MissionsHolder {
     private static List<Mission> mockMissions = new ArrayList<>();
 
     static {
-        mockMissions.add(mission("2-simple", command("takeoff"), command("hover", 2000),  command("land")));
+        mockMissions.add(mission("0-simple", command("takeoff"), command("hover", 2000),  command("land")));
         mockMissions.add(mission("1-verification-mission", command("takeoff"), command("hover", 2000), command("takePicture"), command("forward", 3),  command("hover", 1000), command("takePicture"),  command("hover", 1000),  command("land")));
+        mockMissions.add(mission("2-pictures-mission", command("takeoff"), command("hover", 2000), command("takePicture"), command("cw", 90), command("hover", 2000), command("takePicture"), command("cw", 90), command("hover", 2000), command("takePicture"), command("cw", 90), command("hover", 2000), command("takePicture"), command("cw", 90), command("hover", 2000), command("land")));
         /*mockMissions.add(mission("2-picture", command("wait", 1000), command("takePicture"), command("wait", 1000)));
         mockMissions.add(mission("2-picture", command("wait", 1000), command("takePicture"), command("wait", 1000)));
         mockMissions.add(mission("6-forw-back", command("takeoff"), command("forward", 1),
