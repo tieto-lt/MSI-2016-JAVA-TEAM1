bd70aaa51f57669599d323cc4a881c18d91de78d
Elvinas Valenas <elvinas.val@gmail.com>
2016-08-18 15:24:07 +0300

    [ux] fix after revert

--- src/main/app/main_module.js
+++ src/main/app/main_module.js
@@ -7,9 +7,14 @@ require('angular-jwt');
 require('angular-ui-bootstrap');
 
 
+require('jquery');
+require("bootstrap-material-design");
 
 require("bootstrap/dist/css/bootstrap.css");
+require("bootstrap-material-design/dist/css/bootstrap-material-design.css");
+require("bootstrap-material-design/dist/css/ripples.css");
 
+$.material.init()
 var _module = angular.module('AngularSpringRestDemo', ['ui.router', 'ngMessages', 'ngCookies', 'angular-jwt', 'ui.bootstrap']);
 
 module.exports = _module;
