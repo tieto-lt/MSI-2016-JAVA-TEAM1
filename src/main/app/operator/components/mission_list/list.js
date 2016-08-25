591bfc30836fa6ad5ec8e12b2d8fec057763bcee
markrudenkov <mark.rudenkov@gmail.com>
2016-08-25 16:21:40 +0300

    [32][BUG] Check if mission outcomes works fixed Operator video loading

--- src/main/app/operator/components/mission_list/list.js
+++ src/main/app/operator/components/mission_list/list.js
@@ -3,15 +3,16 @@ var module = require('main_module');
 function Controller($scope, MissionService) {
 
     var vm = this;
+    vm.missionNames={};
     vm.missions = {};
     vm.information = {};
     vm.results = {};
     vm.message = false;
     $scope.oneAtATime = true;
-
+    vm.getResults= getResults;
     vm.publish = publish;
     vm.redo = redo;
-
+    vm.videoUrl = undefined;
     vm.error = '';
     vm.success = '';
 
@@ -25,45 +26,67 @@ function Controller($scope, MissionService) {
         };
 
     function _loadList() {
-        MissionService.getAllMissionsResultsByOperatorID().then(
-            function (response) {
-                vm.missions = response.data;
-                if(vm.missions.length == 0){
-                vm.message = true;
-                console.log(vm.message);
-                }
+          MissionService.getMissionNames().then(
+                function (response) {
+                    vm.missionNames = response.data;
+                    console.log(vm.missionNames);
+                    if(vm.missionNames.length == 0){
+                    vm.message = true;
+                    }
 
-                vm.missions.forEach(function (mission) {
-
-                    mission.startNavigationData.x = mission.startNavigationData.x || 0;
-                    mission.startNavigationData.y = mission.startNavigationData.y || 0;
-                    mission.startNavigationData.z = mission.startNavigationData.z || 0;
-
-                    mission.finishNavigationData.x = mission.finishNavigationData.x || 0;
-                    mission.finishNavigationData.y = mission.finishNavigationData.y || 0;
-                    mission.finishNavigationData.z = mission.finishNavigationData.z || 0;
-
-                    mission.startNavigationData.x = Math.round( mission.startNavigationData.x*100)/100;
-                    mission.startNavigationData.y = Math.round( mission.startNavigationData.y*100)/100;
-                    mission.startNavigationData.z = Math.round( mission.startNavigationData.z*100)/100;
-                    mission.finishNavigationData.x = Math.round( mission.finishNavigationData.x*100)/100;
-                    mission.finishNavigationData.y = Math.round( mission.finishNavigationData.y*100)/100;
-                    mission.finishNavigationData.z = Math.round( mission.startNavigationData.z*100)/100;
+                    $scope.status = false;
+                },
+                function (err) {
+                    console.log('Error',err);
                 });
 
-
-                $scope.status = false;
-            },
-            function (err) {
-                console.log('Error',err);
-            });
      }
+
      $scope.status = {
        isCustomHeaderOpen: false,
        isFirstOpen: true,
        isFirstDisabled: false
      };
 
+    function getResults (name){
+
+         MissionService.getMissionsResultsByName(name).then(
+                    function (response) {
+                        vm.missions = response.data;
+                        console.log(vm.missions);
+                        if(vm.missions.length == 0){
+                        vm.message = true;
+                        console.log(vm.message);
+                        }
+
+                        vm.missions.forEach(function (mission) {
+
+                            mission.startNavigationData.x = mission.startNavigationData.x || 0;
+                            mission.startNavigationData.y = mission.startNavigationData.y || 0;
+                            mission.startNavigationData.z = mission.startNavigationData.z || 0;
+
+                            mission.finishNavigationData.x = mission.finishNavigationData.x || 0;
+                            mission.finishNavigationData.y = mission.finishNavigationData.y || 0;
+                            mission.finishNavigationData.z = mission.finishNavigationData.z || 0;
+
+                            mission.startNavigationData.x = Math.round( mission.startNavigationData.x*100)/100;
+                            mission.startNavigationData.y = Math.round( mission.startNavigationData.y*100)/100;
+                            mission.startNavigationData.z = Math.round( mission.startNavigationData.z*100)/100;
+                            mission.finishNavigationData.x = Math.round( mission.finishNavigationData.x*100)/100;
+                            mission.finishNavigationData.y = Math.round( mission.finishNavigationData.y*100)/100;
+                            mission.finishNavigationData.z = Math.round( mission.startNavigationData.z*100)/100;
+                        });
+
+
+                        $scope.status = false;
+                        vm.videoUrl = "/api/missionsUI/video/" + vm.missions[0].id;
+                        
+
+                    });
+
+
+
+        }
 
     function publish(index) {
             MissionService.publish(vm.missions[index].orderId).then(
